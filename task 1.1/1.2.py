d41ed13a0c82bcff17f8c935f4c37bf4afec3515
yullchikk <jzalutskaya06@gmail.com>
2025-09-12 20:29:58 +0300

    Update 1.2.py

--- task 1.1/1.2.py
+++ task 1.1/1.2.py
@@ -1,1 +1,16 @@
 #создание списка 
+
+# Список чисел
+numbers = [1, 2, 3, 4, 5]
+# Список строк
+fruits = ["яблоко", "банан", "апельсин"]
+print(numbers)  # [1, 2, 3, 4, 5]
+print(fruits)   # ['яблоко', 'банан', 'апельсин']
+
+#создание стека 
+
+stack = []
+stack.append('A')
+stack.append('B')
+stack.append('C')
+print(f"Стек: {stack}")  
