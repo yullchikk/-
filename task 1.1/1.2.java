fedab25b8a8fe8fac7eb41b9447026e7e142ab64
yullchikk <jzalutskaya06@gmail.com>
2025-09-12 20:45:42 +0300

    Update 1.2.java

--- task 1.1/1.2.java
+++ task 1.1/1.2.java
@@ -1,1 +1,17 @@
-# создание списка
+// создание списка
+import java.util.ArrayList;
+
+public class MinimalList {
+    public static void main(String[] args) {
+        // Создаем список
+        ArrayList<String> fruits = new ArrayList<>();
+        
+        // Добавляем элементы
+        fruits.add("Яблоко");
+        fruits.add("Груша");
+        fruits.add("Вишня");
+        
+        // Выводим
+        System.out.println(fruits);
+    }
+}
